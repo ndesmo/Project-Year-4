f9f00744929828a9470e4a435d88c36103182787
ndesmo <nicholas.p.desmond@gmail.com>
2012-11-25 15:12:57 +0000

    25 Nov 12

--- contour.py
+++ contour.py
@@ -3,7 +3,7 @@ from scipy.linalg import eig,svd,norm,schur
 import matplotlib.pyplot as plt
 import scipy.io as sio
 
-mat_contents = sio.loadmat('NLEVP/qep1.mat')
+mat_contents = sio.loadmat('NLEVP/bicycle.mat')
 a0 = mat_contents['a0']
 a1 = mat_contents['a1']
 a2 = mat_contents['a2']
@@ -14,12 +14,12 @@ X = mat_contents['X']
 
 m = a0.shape[0]
 
-lmin = m
+lmin = 1
 
 A = random.rand(m,m) -1/2
 
-N = 40
-R = 5.0
+N = 5
+R = 10.0
 
 mu = 0.+0.j
 
@@ -27,7 +27,7 @@ def T(z):
     #return z*identity(m, dtype=complex)
     #return z*identity(m, dtype=complex)-A
     #return (z**4)*a4+(z**3)*a3+(z**2)*a2+z*a1+a0
-    return (z**2)*a0+z*a1+a2
+    return (z**2)*a2+z*a1+a0
 
 for l in range(lmin,m+1):
     
@@ -51,7 +51,6 @@ for l in range(lmin,m+1):
     if k!=l:
         break
     
-print k
 # compute A1
 
 A1=zeros([m,l], dtype=complex)
@@ -63,8 +62,6 @@ for i in range(N):
     for a in range(l):
         dA[:,a]=linalg.solve((T(phi)),B[:,a])
     A1 += dA
-
-A1 += mu*A0
 
 V0 = V[:m,:k]       # trim matrices
 W0h = Wh[:k,:l]
@@ -78,45 +75,41 @@ Sinv = diag(1/s)
 B = dot(dot(dot(V0h,A1),W0),Sinv) # calculate B
 
 lambs,svects = eig(B) # calc eigenvalues and vectors of B, and A for comparison
+lambs = lambs - mu
 lam = e ; vec = X
+
 print lambs
-print e
 
 # are all eigenvalues in the contour?
 failed = False
 tolres = 1e-6
-vects = zeros((k,k),dtype=complex)
+vects = zeros((m,k),dtype=complex)
+deletelist = []
 for a in range(k):
     if abs(lambs[a]-mu)>=R:
         failed = True
-        #print "abs",lambs[a]
-        break
+        deletelist.append(a)
+        print "outside range"
+        continue
     else:
+        # satisfies original equation?
         vects[:,a] = dot(V0,svects[:,a])
         test = norm(dot(T(lambs[a]),vects[:,a]))
         if test>tolres:
             failed = True
-            #print "tolres",test
-            break
+            print "norm is",test
+            deletelist.append(a)
 
-"""        
-print lambs, norm(vects)
-print lam,norm(vec)
-print sqrt(m)
-"""
+
 print failed
+
+print lam
 
 if failed: # if it failed either of the two above checks then schur decompose
 
     U,Q = schur(B, output='complex') # schur decompose 
     
-    deletelist = []
-    for a in range(k):
-        if abs(lambs[a]-mu)>=R:
-            deletelist.append(a)
-    lambs = delete(a,deletelist)
-    Q = delete(Q,deletelist,1)
-    
-    #print Q
             
-
+    lambs = delete(lambs,deletelist)
+    print lambs
+    Q = delete(Q,deletelist,1)
